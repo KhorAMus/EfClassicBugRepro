65bdeeca32b42135c694971db35f41dc8fd3b044
KhorAMus <khoramus@yandex.ru>
2026-09-04 22:08:45 +0500

    add a hint

--- Models/BugRecord.cs
+++ Models/BugRecord.cs
@@ -5,4 +5,7 @@ public class BugRecord
     public int Id { get; set; }
 
     public string Value { get; set; } = string.Empty;
+
+    //Uncomment the following line after first run:
+    //public bool TestField { get; set; }
 }
